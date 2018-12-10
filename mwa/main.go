b23b8c2221108636efb4a8dc9f2300b158e8055a
Jormen Janssen <j.janssen@riwo.eu>
2018-12-10 16:05:24 +0100

    Configurable options

--- mwa/main.go
+++ mwa/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	nh := NetworkHealth{Address: "192.168.10.122"}
+	delay := flag.Duration("delay", 30*time.Second, "The duration to delay after an succesfull attempt")
+	retryDelay := flag.Duration("retryDelay", 5*time.Second, "The seconds to delay after an succesfull attempt")
+	retries := flag.Int("retries", 3, "The number of retries before giving up")
+	host := flag.String("host", "192.168.10.2", "The default host to check for")
+
+	flag.Parse()
+
+	nh := NetworkHealth{Address: *host, Delay: *delay, Retries: *retries, RetryDelay: *retryDelay}
+
+	fmt.Println(fmt.Printf("Running network health against target: %v with interval: %v and max: %v retries (%v delay between failed attempts )", nh.Address, nh.Delay, nh.Retries, nh.RetryDelay))
 
 	for {
 		err := KeepAlive(nh.VerifyConnection, nh.ShouldRetry)
--- mwa/network.go
+++ mwa/network.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 type NetworkHealth struct {
-	Address string
+	Address    string
+	Delay      time.Duration
+	RetryDelay time.Duration
+	Retries    int
 }
 
 func (nh *NetworkHealth) VerifyConnection() error {
 	fmt.Println(fmt.Printf("Invoking ping to: %v", nh.Address))
 	_, err := Ping(nh.Address)
+
+	if err == nil {
+		<-time.After(nh.Delay)
+	}
+
 	return err
 }
 
 func (nh *NetworkHealth) ShouldRetry(count int, err error) bool {
-	return (count < 20)
+
+	if count < nh.Retries {
+		<-time.After(nh.RetryDelay)
+	}
+
+	return (count < nh.Retries)
 }
