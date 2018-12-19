4e93eb2edd84b84a3d13bff5b0afaf9fb8c04456
Jormen Janssen <j.janssen@riwo.eu>
2018-12-19 16:49:41 +0100

    Added more debug logging for gateway detection

--- mwa/autotarget.go
+++ mwa/autotarget.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	netlink "github.com/jormenjanssen/netlink"
+	log "github.com/sirupsen/logrus"
 )
 
 func GetIpv4TargetForAdapterGatewayWithTimeout(adapter string, duration time.Duration) (net.IP, error) {
@@ -13,11 +14,17 @@ func GetIpv4TargetForAdapterGatewayWithTimeout(adapter string, duration time.Dur
 
 	for time.Since(startTime) < duration {
 
-		ip, _ := GetIpv4TargetForAdapterGateway(adapter)
+		ip, err := GetIpv4TargetForAdapterGateway(adapter)
+
+		if err != nil {
+			log.Debugf("Failed to detect route error: %v", err)
+		}
 
 		if ip != nil {
 			return ip, nil
 		}
+
+		<-time.After(1 * time.Second)
 	}
 
 	return nil, TimeOutError
@@ -30,9 +37,16 @@ func GetIpv4TargetForAdapterGateway(adapter string) (net.IP, error) {
 	if err == nil {
 
 		for _, route := range routes {
-			if route.Iface.Name == adapter && route.IPNet.IP.To4() != nil {
-				return route.IPNet.IP, nil
+
+			if err != nil {
+
+				log.Debugf("Detected route for adapter: %v with IP: %v IsIpv4: %v", route.Iface.Name, route.IPNet.IP, route.IPNet.IP.To4() != nil)
+				if route.Iface.Name == adapter && route.IPNet.IP.To4() != nil {
+					return route.IPNet.IP, nil
+				}
 			}
+
+			routes, err = netlink.NetworkGetRoutes()
 		}
 	}
 
