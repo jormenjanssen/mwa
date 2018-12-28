bf33c9c5f647f048184659c5ce384f9f3f9e65cd
Jormen Janssen <J.janssen@riwo.eu>
2018-12-28 11:38:00 +0100

    More logging and first build linux amd64 for faster ci results

--- build.sh
+++ build.sh
@@ -7,22 +7,24 @@ export GIT_COMMIT=$(git rev-list -1 HEAD)
 export GIT_TAG=$(git describe --tags --abbrev=0)
 
 echo "Invoking build script with GIMME_ARCH: ${GIMME_ARCH}"
+echo "GIT COMMIT: ${GIT_COMMIT}"
+echo "GIT VERSION: ${GIT_TAG}"
 
 if [[ $GIMME_OS == 'windows' ]]; then
-    go build -ldflags "-X main.GitCommit=${GIT_COMMIT} main.Version=${GIT_TAG}"
+    go build -ldflags "-X main.GitCommit=${GIT_COMMIT} -X main.Version=${GIT_TAG}"
 	exit 0
 fi
 
 if [[ $GIMME_ARCH == 'amd64' ]]; then
     echo "Running golang test for linux-x64"
     go test -v ./...
-    go build -ldflags "-X main.GitCommit=${GIT_COMMIT} main.Version=${GIT_TAG}"
+    go build -ldflags "-X main.GitCommit=${GIT_COMMIT} -X main.Version=${GIT_TAG}"
     mv mwa mwa64
 fi
 
 if [[ $GIMME_ARCH == 'arm' ]]; then
     echo "Running golang release build for linux-arm"
-    go build -ldflags "-s -w -X main.GitCommit=${GIT_COMMIT} main.Version=${GIT_TAG}"
+    go build -ldflags "-s -w -X main.GitCommit=${GIT_COMMIT} -X main.Version=${GIT_TAG}"
 fi
 
 
