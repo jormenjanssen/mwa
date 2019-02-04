728b62296cf6c861de572eaf35602913fc3ee996
Jormen Janssen <j.janssen@riwo.eu>
2019-02-04 16:13:53 +0100

    Add recovery action logging

--- mwa/script_linux.go
+++ mwa/script_linux.go
@@ -13,6 +13,11 @@ func ExecuteScript(invokeScript string) error {
 
 	if cmdErr != nil {
 		log.Warnf("Failed to execute script %v", cmdErr)
+	} else {
+		out, err := cmd.Output()
+		if err != nil {
+			log.Infof("Command output: %v", out)
+		}
 	}
 
 	return cmdErr
