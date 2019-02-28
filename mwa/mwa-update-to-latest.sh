f3ef90f4762edf1e26b0ec6210441cda8d2835fe
Jormen Janssen <J.janssen@riwo.eu>
2019-02-28 15:45:45 +0100

    Arm upgrade script

---
+++ mwa/mwa-update-to-latest.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+echo "Preparing update"
+systemctl stop wireless-agent
+curl -L https://rimotedeployment.blob.core.windows.net/downloads/Wireless-Agent/mwa-1.15-arm > /usr/bin/mwa
+curl -L https://rimotedeployment.blob.core.windows.net/downloads/Wireless-Agent/reset-wifi.sh > /usr/bin/reset-wifi.sh
+systemctl start wireless-agent
+echo "Updated"
