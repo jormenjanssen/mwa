388437095346647950fd37765d067daf59937c17
Jormen Janssen <j.janssen@riwo.eu>
2019-03-01 08:48:52 +0100

    Update to 1.16

--- mwa/mwa-update-to-latest.sh
+++ mwa/mwa-update-to-latest.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Preparing update"
 systemctl stop wireless-agent
-curl -L https://rimotedeployment.blob.core.windows.net/downloads/Wireless-Agent/mwa-1.15-arm > /usr/bin/mwa
+curl -L https://rimotedeployment.blob.core.windows.net/downloads/Wireless-Agent/mwa-1.16-arm > /usr/bin/mwa
 curl -L https://rimotedeployment.blob.core.windows.net/downloads/Wireless-Agent/reset-wifi.sh > /usr/bin/reset-wifi.sh
 systemctl start wireless-agent
 echo "Updated"
